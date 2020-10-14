cf51d60ea9dea957e06cdd53ea678850b57d28ae
Mikhail Zinovkin <zmikel@mail.ru>
2020-10-14 05:16:21 +0300

    bug

--- drivers/media/platform/mxc/capture/ipu_vdi_enc.c
+++ drivers/media/platform/mxc/capture/ipu_vdi_enc.c
@@ -407,8 +407,9 @@ static int vdi_enc_setup(cam_data *cam)
  *
  * @return  status
  */
-static int vdi_enc_eba_update(struct ipu_soc *ipu, dma_addr_t eba, int *buffer_num)
+static int vdi_enc_eba_update(void *private, struct ipu_soc *ipu, dma_addr_t eba, int *buffer_num)
 {
+    cam_data *cam = (cam_data *) private;
 	int err = 0;
 
 	pr_debug("eba %x\n", eba);
